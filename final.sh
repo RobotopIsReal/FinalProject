bac090d78dbc23c5e21c9c1e44f9ff87da3a68e9
RobotopIsReal <nasim.alikishibekov@gmail.com>
2026-01-28 15:16:07 -0600

    Defined the player variables at the start of the script. Added a line in the shop prompt that displays your stats

--- final.sh
+++ final.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+#player stats
+gold=0
+atk=1
+def=0
+hp=5
+critchance=8
 dungeon_begin(){
 	echo "You enter Floor $floor.
 	|| 1) Enter shop
 	|| 2) Fight $monster ($monsteratk|$monsterhp)
-	|| 3) Loot chest
-	|| 4) Check your stats"
+	|| 3) Loot chest"
 	read -p "Enter selection: " input
 	send_to_choice
 }
@@ -13,8 +18,10 @@ enter_shop(){
 	echo "Welcome to the shop!
 	|| 1) Sword upgrade (+1 ATK) - 10G
 	|| 2) Armor upgrade (+1 HP) - 10G
-	|| 3) Shield upgrade (+1 DEF) - 30G
-	|| 4) Return"
+	|| 3) Skill upgrade (+2% CRIT) - 10G
+	|| 4) Shield upgrade (+1 DEF) - 30G
+	|| 5) Return
+	|| Stats: $atk Attack | $hp Health | $def Defense | $critchance% CritChance"
 	read -p "Choose upgrade: " shop_input
 }
 
